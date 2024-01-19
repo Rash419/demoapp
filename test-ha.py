9a8c17c00a2606e4becd8e8191874f2b646ee188
Rashesh <rasheshpadia419@gmail.com>
2024-01-19 12:38:56 +0530

    update test-ha.py
    - added test_serverid_mismatch() function
    - pass base url in argument

--- test-ha.py
+++ test-ha.py
@@ -1,13 +1,19 @@
 import requests
 import json
 import secrets
+import sys
 
+url = f"{sys.argv[1]}/serverId?RouteToken="
+print(f"starting script with url[{url}]")
 def create_serverid_map():
-    url = "http://test.demoapp:30080/serverId?RouteToken="
     for i in range(1000):
        route_token = generate_random_token()
        serverid = get_serverid(f"{url}{route_token}")
        serverid_map[serverid] = route_token
+
+    print("Map of serverid<->route_token:")
+    for key,value in serverid_map.items():
+        print(f"{key}<->{value}")
 
 
 def generate_random_token():
@@ -25,8 +31,17 @@ def get_serverid(url):
     else:
         print('Failed to get serverid, HAproxy returned status_code ' + str(response.status_code))
 
+def test_serverid_mismatch():
+    print('Testing for serverId mismatch...')
+    for expected_serverid, route_token in serverid_map.items():
+       actual_serverid = get_serverid(f"{url}{route_token}") 
+       if expected_serverid != actual_serverid:
+            print(f"Mismatch for route_token[{route_token}], expected_serverid[{expected_serverid}] but actual_serverid[{actual_serverid}]")
+            return
+    print('No mismatch found')
+
 serverid_map = {}
 create_serverid_map()
-print("Map of serverid<->route_token:")
-for key,value in serverid_map.items():
-    print(f"{key}<->{value}")
+
+# test mismatch by sending /serverId request with route_token from map and check if we get same serverId
+test_serverid_mismatch()
